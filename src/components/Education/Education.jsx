a313b469f3ee231855083b5d3eca775551f6c9e8
unknown <ce.rumiakter@gmail.com>
2025-02-13 16:12:39 +0600

    add education section

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,4 +1,5 @@
-import { FaGraduationCap } from "react-icons/fa";
+import { FaGraduationCap, FaBook } from "react-icons/fa";
+
 
 const Education = () => {
     return (
@@ -7,11 +8,25 @@ const Education = () => {
                 <h1 className='text-2xl font-medium'>Welcome to Education</h1>
                 <p>We are Expert in Education</p>
                 <p>Lorem ipsum dolor sit amet, cons ectetur elit. Vestibulum nec odios Suspe ndisse cursus mal suada faci lisis. Lorem ipsum dolor sit ametion consectetur elit. Vesti bulum nec odio ipsum ons ectetur elit. Vestibulum nec odios Suspe ipsum dolor sit amet, cons ectetur elit. Vestibulum nec odios Suspe ndisse cursus mal ipsum dolor sit amet, cons ectetur elit.</p>
-                <div>
-                    <div className="flex items-center gap-4">
+                <div className="flex items-center gap-8 mt-8">
+                    <div className="flex items-center p-4 gap-4 border-t-4 border-t-amber-400 bg-white">
                         <span className="text-6xl"><FaGraduationCap /></span>
                         <div>
                             <h1 className="text-lg font-medium">Trending Courses</h1>
+                            <p className="text-base text-[#666666]">Lorem ipsum dolor sit amet, cons ectetur ellt. Vestibulum nec </p>
+                        </div>
+                    </div>
+                    <div className="flex items-center p-4 gap-4 border-t-4 border-t-amber-400 bg-white">
+                        <span className="text-6xl"><FaGraduationCap /></span>
+                        <div>
+                            <h1 className="text-lg font-medium">Online Courses</h1>
+                            <p className="text-base text-[#666666]">Lorem ipsum dolor sit amet, cons ectetur ellt. Vestibulum nec </p>
+                        </div>
+                    </div>
+                    <div className="flex items-center p-4 gap-4 border-t-4 border-t-amber-400 bg-white">
+                        <span className="text-6xl"><FaGraduationCap /></span>
+                        <div>
+                            <h1 className="text-lg font-medium">Books & Library</h1>
                             <p className="text-base text-[#666666]">Lorem ipsum dolor sit amet, cons ectetur ellt. Vestibulum nec </p>
                         </div>
                     </div>
